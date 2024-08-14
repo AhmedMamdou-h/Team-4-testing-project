fae59f365d390208a4e9ad443fb3d29c4c3a354a
fekry zakaria <fekry.zakaria1461998@gmail.com>
2024-08-14 13:35:27 +0200

    feky is here

---
+++ src/app/services/category.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  constructor() { }
+  getBadwayById(){
+    console.log('yaaaaa badawy ya negm');
+    
+  }
+ 
+}
