7c2dc7675d3102fc1c3016eb7452733e3d667bf6
Samar Eissa <samareissa469@gmail.com>
2024-08-14 14:38:04 +0300

    adding new function in ts file in componant homePage commit

--- src/app/componantSamar/home-page/home-page.component.ts
+++ src/app/componantSamar/home-page/home-page.component.ts
@@ -8,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrl: './home-page.component.css'
 })
 export class HomePageComponent {
-
+ gotoproductPage(){
+  console.log(" function gotoproductPage");
+  
+ }
 }
